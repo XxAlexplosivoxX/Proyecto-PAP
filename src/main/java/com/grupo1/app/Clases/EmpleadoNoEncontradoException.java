afe15da307ce828fe29ecd6710fd6b084cdcc4cb
sp26003-dotcom <sp26003@ues.edu.sv>
2026-05-21 10:59:50 -0600

    git push origin main

---
+++ src/main/java/com/grupo1/app/Clases/EmpleadoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.grupo1.app.Clases;
+
+public class EmpleadoNoEncontradoException extends Exception {
+    public EmpleadoNoEncontradoException(String message) {
+        super(message);
+    }
+}
